493fa900d61771c96f07cfcad73d2aded533a030
berz_ <quentin@qvdb.be>
2014-05-05 00:46:44 +0200

    Simplify code: only store one scan per blad.

--- lossebladjes/database.py
+++ lossebladjes/database.py
@@ -2,10 +2,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 
 from lossebladjes import app
 
-
 db = SQLAlchemy(app)
-
-# db.create_all()
 
 class Blad(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -15,13 +12,9 @@ class Blad(db.Model):
     toegevoegd_op = db.Column(db.String())
     voorgedragen_op = db.Column(db.String())
     eigenaar = db.Column(db.String())
-    scans = db.relationship('Scan', backref=db.backref('blad'))
+    scan_filename = db.Column(db.String)
     extra = db.Column(db.String())
 
-class Scan(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    filename = db.Column(db.String)
-    blad_id = db.Column(db.Integer, db.ForeignKey('blad.id'))
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
